2052f62543aeaecd3f6df0e4b6930fb669a263f0
Darkhan Turysbekov <da.turysbek@gmail.com>
2022-10-19 22:18:29 +0600

    Initial commit

---
+++ FlickrSearch/FlickrFiles/FlickrSearchResults.swift
@@ -0,0 +1,10 @@
+//
+//  Created by darkhan on 03.09.2022.
+//
+
+import Foundation
+
+struct FlickrSearchResults {
+  let searchTerm: String
+  let searchResults: [FlickrPhoto]
+}
---
+++ FlickrSearch/FlickrPhotoCell.swift
@@ -0,0 +1,9 @@
+//
+//  Created by darkhan on 03.09.2022.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+  @IBOutlet weak var imageView: UIImageView!
+}
